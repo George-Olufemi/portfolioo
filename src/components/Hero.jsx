9d8bc2fb75932ae91df3ebb560223f74c8e0af05
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-15 19:24:13 +0100

    Modified the Hero Section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <video className='w-screen' src={assets} autoPlay loop muted />
         <div className="flex justify-center items-center">
             <div className='pt-16 md:pt-64 md:leading-loose content absolute top-0 flex justify-center items-center text-white'>
-                <h1 className='text-center text-2xl md:text-7xl px-16 md:px-10 font-semibold md:font-bold tracking-wider'>My name is George Olufemi && I'm a<br /> Front-end Web Developer.</h1>
+                <h1 className='text-center text-2xl md:text-5xl px-16 md:px-10 font-semibold md:font-bold tracking-wider'>My name is George Olufemi && I'm a<br /> Front-end Web Developer.</h1>
             </div>
         </div>
     </div>
